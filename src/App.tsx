6a5bbc5b1bfa0442330201add85bb96098a48faf
Danina Valeriya <valeriya.danina@extern.a1.at>
2022-11-08 17:20:36 +0100

    added trainCard component, example of json data

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import shoppingCart from './images/shopping_cart_black_24dp 1.png';
 import './App.css';
+import {Header} from "./Header";
+import {TrainConnections} from "./TrainConnections";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="header">
-          <img className="App-logo" alt="Logo" />
-            <span className="LanguageChange"> SK/EN </span>
-            <div className="IconLayout">
-                <img className="CartIcon" src={shoppingCart} />
-            </div>
-            <div>
-                <button className="SignButton" type="button">Sign in</button>
-            </div>
-        </div>
-      </header>
+        <Header />
+        <TrainConnections />
     </div>
   );
 }
