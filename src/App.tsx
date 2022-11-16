de19a2c6751f4377800124c425347a3830d4085b
luboskris <103902858+luboskris@users.noreply.github.com>
2022-11-16 18:49:10 +0100

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Footer } from './Footer';
 import {Header} from "./Header";
 import {TrainConnections} from "./TrainConnections";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
         <Header />
         <TrainConnections />
+        <Footer />
     </div>
   );
 }
