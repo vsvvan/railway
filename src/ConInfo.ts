129e0419381ece36d2bac140901da1cd60575ea8
Peter <horvath203@uniba.sk>
2022-11-23 01:03:35 +0100

    SearchBar added to App.tsx and restyled

---
+++ src/ConInfo.ts
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default interface ConInfor {
+    from: string,
+    to: string,
+    month: number,
+    day: number,
+    hour: number,
+    minute: number
+}
--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,31 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './SearchBar.css';
 import { useForm, SubmitHandler } from "react-hook-form";
+import ConInfo from "./ConInfo";
 
 type FormValues = {
     from: string,
     to: string,
     day: number,
     month: number,
-    hours: number,
-    minutes: number
-  };
+    hour: number,
+    minute: number
+};
 
-export function SearchBar() {
+interface SearchBarProps{
+    dispatch: React.Dispatch<React.SetStateAction<ConInfo>>
+}
+
+export function SearchBar(props: SearchBarProps) {
     const DefVals = () => {
         const curD = new Date();
         const defVal: FormValues = {
             from: '',
             to: '',
             day: curD.getDate(),
-            month: curD.getMonth(),
-            hours: curD.getHours(),
-            minutes: curD.getMinutes()
+            month: curD.getMonth()+1,
+            hour: curD.getHours(),
+            minute: curD.getMinutes()
         };
         return defVal;
     }
     const { register, handleSubmit, getValues, setValue, formState: { errors } } = useForm<FormValues>({ defaultValues: DefVals() });
     const onSubmit: SubmitHandler<FormValues> = data => {
+        props.dispatch({
+            from: data.from,
+            to: data.to,
+            month: data.month,
+            day: data.day,
+            hour: data.hour,
+            minute: data.minute
+        })
         console.log(data);
     }
     const SwapDestinations = () => {
@@ -39,20 +52,22 @@ export function SearchBar() {
             <h3 className='searchTitle'>Connection search</h3>
 
             <form onSubmit={handleSubmit(onSubmit)} className="searchForm">
-                <input className='from' placeholder="from" {...register("from", { required: true })} />
+                <input className='from' placeholder="From" {...register("from", { required: true })} />
                 {errors.from && <span className="fromError">Enter name of the starting station</span>}
-                <button className='swapbtn' onClick={SwapDestinations}></button>
-                <input className='to' placeholder="to" {...register("to", { required: true })} />
+
+                <button className='swapbtn' onClick={SwapDestinations}>swap</button>
+
+                <input className='to' placeholder="To" {...register("to", { required: true })} />
                 {errors.to && <span className="toError">Enter name of the destination station</span>}
-                <input className='day' type="number" {...register("day", { min:1, max: 31, required: true })}/>
+
+                <input className="day" type="number" {...register("day", { min:1, max: 31, required: true })}/>
                 <input className='month' type="number" {...register("month", { min:1, max: 12, required: true })}/>
-                <input className='hours' type="number" {...register("hours", { min:0, max: 23, required: true })}/>
-                <input className='minutes' type="number" {...register("minutes", { min:0, max: 59, required: true })}/>
-                {(errors.day || errors.month || errors.hours || errors.minutes) && <span className="timeError">Enter a correct date and time of desired departure</span>}
+                <input className='hour' type="number" {...register("hour", { min:0, max: 23, required: true })}/>
+                <input className='minute' type="number" {...register("minute", { min:0, max: 59, required: true })}/>
+                {(errors.day || errors.month || errors.hour || errors.minute) && <span className="timeError">Enter a correct date and time of desired departure</span>}
 
                 <input className='submitbtn' value="Search Connections" type="submit" />
             </form>
         </div>
-
     );
 }
