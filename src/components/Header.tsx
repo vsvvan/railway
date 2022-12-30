396584eb343f3b6d8a98291e025319f18639744f
luboskris <103902858+luboskris@users.noreply.github.com>
2022-12-30 11:12:17 +0100

    l

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,10 +8,45 @@ export const Header = () => {
   const navigate = useNavigate();
   return (
     <header className="App-header">
-      <div className="header">
+      <div className="HeaderMainPage">
+
         <Button onClick={() => navigate('/railway/')}>
           <img className="App-logo" src={Logo} alt="Logo" />
         </Button>
+        
+        <div className='Menu-bar'>
+          <ul>
+          <li><a href="#">Ticket Purchase</a>
+            <div className='Sub-menu-1'>
+            <ul>
+              <li><a href="#">Security rules</a></li>
+              <li><a href="#">Purchase of one-way travel ticket</a></li>
+              <li><a href="#">Purchase of Motorail ticket </a></li>
+              <li><a href="#">Cancellation online</a></li>
+              <li><a href="#">Conditions of payments</a></li>
+              <li><a href="#">Purchase of season ticket - Tatry</a></li>
+              <li><a href="#">Online sale terms and conditions </a></li>             
+            </ul>
+            </div>
+        </li>
+        <li>Timetable</li>
+        <li>Tickets and Offers</li>
+        <li> <a href="#">Sevices</a>
+        <div className='Sub-menu-1'>
+        <ul>
+              <li><a href="#">Security rules</a></li>
+              <li><a href="#">Purchase of one-way travel ticket</a></li>
+              <li><a href="#">Cancellation online</a></li>
+              <li><a href="#">Conditions of payments</a></li>
+              <li><a href="#">Security rules</a></li>
+              <li><a href="#">Online sale terms and conditions </a></li>
+            </ul>
+        </div>
+          </li>
+        <li>Career</li>
+        <li>About us</li>
+        </ul>
+        </div>
         <div className="HeaderContainer">
           <span className="LanguageChange"> SK/EN </span>
           <div className="IconLayout">
---
+++ src/components/HeaderMainPage.tsx
@@ -0,0 +1,24 @@
+import shoppingCart from '../images/shopping_cart_black_24dp 1.png';
+import React from 'react';
+import Logo from '../images/Logo-header.svg';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
+
+export const Header = () => {
+  const navigate = useNavigate();
+  return (
+    <header className="App-header">
+      <div className="header">
+        <Button onClick={() => navigate('/railway/')}>
+          <img className="App-logo" src={Logo} alt="Logo" />
+        </Button>
+        <div className="HeaderContainer">
+          <span className="LanguageChange"> SK/EN </span>
+          <div className="IconLayout">
+            <img className="CartIcon" src={shoppingCart} alt="" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
